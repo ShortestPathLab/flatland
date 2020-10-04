f7ec41a885ad79b949f348508bb2267bf5f7c370
nobodyczcz <jianshen858@gmail.com>
2020-10-04 22:34:55 +1100

    update step timeout

--- flatland/evaluators/service.py
+++ flatland/evaluators/service.py
@@ -44,7 +44,7 @@ m.patch()
 ########################################################
 # CONSTANTS
 ########################################################
-PER_STEP_TIMEOUT = 10 * 60 * 10  # 5 minutes
+PER_STEP_TIMEOUT = 10 * 60 * 50  # 5 minutes
 RANDOM_SEED = int(os.getenv("FLATLAND_EVALUATION_RANDOM_SEED", 1001))
 SUPPORTED_CLIENT_VERSIONS = \
     [

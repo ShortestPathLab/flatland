6b856f4a79426dc854b225ccf4024f44335b7642
nobodyczcz <jianshen858@gmail.com>
2021-03-03 11:10:31 +1100

    modify to 2 agent problem

--- examples/find_path.py
+++ examples/find_path.py
@@ -1,5 +1,5 @@
 """
-This is the python script for question 1. In this script, you are required to implement a single agent path-finding algorithm
+This is the python script week 1 tutorial exercise.
 """
 
 
@@ -8,35 +8,25 @@ This is the python script for question 1. In this script, you are required to im
 #########################
 
 # Run this script, required start and goal locations will be printed to terminal.
-# Then replace this empty list with a list of location tuple, so that the agent move following the path from start location to goal location.
+# Then replace empty lists with lists of location tuple, so that each train can following the path to reach goal location.
 # For example : [(2,3),(2,4),(3,4)]
-path = []
+train_1 = []
+train_2 = []
 
-
-
-#########################
-# Debugger and visualizer options
-#########################
-
-print("Start:  (6, 8)  Goal:  (3, 3)")
-
-# Set these debug option to True if you want more information printed
+# Turn debug to True to know what's wrong with your path.
 debug = False
-visualizer = True
 
-# This function return a list of location tuple as the solution.
-# @param start A tuple of (x,y) coordinates
-# @param start_direction An Int indicate direction.
-# @param goal A tuple of (x,y) coordinates
-# @param env The flatland railway environment
-# @return path A list of (x,y) tuple.
-def get_path(start: tuple, start_direction: int, goal: tuple, env):
-    return path
 
 
 #########################
 # You should not modify any codes below. You can read it know how we ran flatland environment.
 ########################
+
+print("Train 1: Start  (6, 8)  Goal  (3, 3)")
+print("Train 2: Start  (6, 0)  Goal  (0, 6)")
+
+def get_path(local_env):
+    return [train_1, train_2]
 
 import glob, os
 
@@ -50,7 +40,7 @@ except:
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 test_cases = glob.glob(os.path.join(script_path,"test_0.pkl"))
-evaluator(get_path,test_cases,debug,visualizer,1)
+evaluator(get_path,test_cases,debug,True,3)
 
 
 
